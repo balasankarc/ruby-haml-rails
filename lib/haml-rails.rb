450ae361dde7ef4fd6247daaa642601767bb5159
Balasankar C <balasankarc@autistici.org>
2015-03-24 17:05:07 +0530

    Imported Upstream version 0.9.0

--- lib/haml-rails.rb
+++ lib/haml-rails.rb
@@ -4,15 +4,12 @@ require 'haml/railtie'
 
 module Haml
   module Rails
+    class Engine < ::Rails::Engine
+    end
     class Railtie < ::Rails::Railtie
-      if ::Rails.version.to_s >= "3.1"
-        config.app_generators.template_engine :haml
-      else
-        config.generators.template_engine :haml
-      end
+      config.app_generators.template_engine :haml
 
       config.before_initialize do
-        Haml.init_rails(binding)
         Haml::Template.options[:format] = :html5
       end
 
